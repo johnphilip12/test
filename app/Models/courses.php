7ffab46e1dff42afd5d5fcbffdfea7de19b86a6d
John Philip Villamor <villamorjohnphilip2@gmail.com>
2021-09-28 19:14:51 +0800

    commit

---
+++ app/Models/courses.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class courses extends Model
+{
+    use HasFactory;
+    use HasFactory;
+    public function trainees(){
+        return $this->hasMany('App\Models\Trainees');
+    }
+}
+
